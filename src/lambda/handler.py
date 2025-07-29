8af97fa712f5e5447fd71572435154ef8ec8ab62
gobrianna <whoamibrianna@tuta.io>
2025-07-29 18:51:37 -0400

    refactor(lambda): remove KMS encryption

--- src/lambda/handler.py
+++ src/lambda/handler.py
@@ -5,12 +5,11 @@ from botocore.exceptions import ClientError # AWS-specific error handling
 
 # client init
 dynamodb = boto3.resource("dynamodb")
-kms = boto3.client("kms")
 
 # reads .env variables set in Lambda config
 TABLE_NAME = os.environ.get("TABLE_NAME")
-KMS_KEY_ID = os.environ.get("KMS_KEY_ID")
 
+# enable support for frontend access
 cors_headers = {
     "Access-Control-Allow-Origin": "https://d3um4w745sq9lj.cloudfront.net",
     "Access-Control-Allow-Headers": "Content-Type",
@@ -18,13 +17,6 @@ cors_headers = {
 }
 
 def lambda_handler(event, context):
-    if event.get("httpMethod") == "OPTIONS":
-        return {
-            "statusCode": 200,
-            "headers": cors_headers,
-            "body": json.dumps({"message": "CORS preflight OK"})
-        }
-
     try:
         # parses incoming request body
         body = json.loads(event.get("body", "{}"))
@@ -40,28 +32,13 @@ def lambda_handler(event, context):
                 "body": json.dumps({"error": "All fields are required."})
             }
 
-        # encrypts message with KMS
-        try:
-            encrypted_response = kms.encrypt(
-                KeyId=KMS_KEY_ID,
-                Plaintext=message.encode("utf-8")
-            )
-            encrypted_message = encrypted_response["CiphertextBlob"]
-        except ClientError as e:
-            print(f"KMS Error: {e}")
-            return {
-                "statusCode": 500,
-                "headers": cors_headers,
-                "body": json.dumps({"error": "Encryption failed."})
-            }
-
-        # stores form data in DynamoDB table
+        # store data in DynamoDB
         try:
             table = dynamodb.Table(TABLE_NAME)
             table.put_item(Item={
                 "email": email,
                 "name": name,
-                "message": encrypted_message.hex()  # stores encrpyted message as hex string
+                "message": message
             })
         except ClientError as e:
             print(f"DynamoDB Error: {e}")
